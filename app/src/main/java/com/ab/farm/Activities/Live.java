e4d74bee782482bdbc9c042285adea7122607756
abdullah <asas3739@gmail.com>
2020-09-24 05:40:20 +0500

    AB

--- app/src/main/java/com/ab/farm/Activities/Live.java
+++ app/src/main/java/com/ab/farm/Activities/Live.java
@@ -3,15 +3,20 @@ package com.ab.farm.Activities;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.MediaController;
+import android.widget.VideoView;
 
 import com.ab.farm.R;
 
 public class Live extends AppCompatActivity {
 
     ImageView back,contactus,notification;
+    VideoView videoView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,5 +48,17 @@ public class Live extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        videoView = (VideoView)findViewById(R.id.videoview);
+
+        String path1="https://youtu.be/LIid42fEr-Q";
+
+        Uri uri=Uri.parse(path1);
+        videoView.setVideoURI(uri);
+        videoView.requestFocus();
+        videoView.start();
+
+
     }
+
 }
